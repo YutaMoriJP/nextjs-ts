f2fac8dc04f097db333ea2170b9cadf83594de9c
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-04 14:14:31 -0700

    build fixes

--- util/asyncReq.ts
+++ util/asyncReq.ts
@@ -1,4 +1,4 @@
-import { Data } from "../index";
+import { Data } from "../pages/blog/index";
 
 export const asyncReq = async (): Promise<Data[]> => {
   const url = "https://jsonplaceholder.typicode.com/users/";
