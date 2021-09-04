d290e387770dfbf8fde07cfde3c7c45b06bf9746
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-04 15:55:20 -0700

    css updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../stores/authContext";
 import CardContainer from "../component/Card/CardContainer";
 import styles from "../styles/Home.module.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 interface Data {
   postId: number;
@@ -15,19 +16,23 @@ interface Res {
   data: Data[];
   msg: string;
   loggedIn: boolean;
+  loading: boolean;
 }
 
 const Home = () => {
   const { user, authReady } = useAuth();
-  const [{ data, msg, loggedIn }, setData] = useState<Res>({
+  const [{ data, msg, loggedIn, loading }, setData] = useState<Res>({
     data: null,
     msg: "",
     loggedIn: false,
+    loading: false,
   });
 
   useEffect(() => {
     let isCanceled = false;
     const asyncReq = async () => {
+      setData(prev => ({ ...prev, loading: true }));
+
       try {
         const controller = new AbortController();
         const signal = controller.signal;
@@ -51,7 +56,7 @@ const Home = () => {
         const data = await res.json();
         if (!isCanceled) {
           console.log(data);
-          setData(data);
+          setData({ ...data, loading: false });
         }
       } catch (error) {
         if (!isCanceled) {
@@ -67,11 +72,17 @@ const Home = () => {
   }, [user, authReady]);
   return (
     <>
+      {loading && (
+        <article className={styles.center}>
+          <CircularProgress color="primary" />
+        </article>
+      )}
       {!loggedIn && msg && (
         <p className={styles.alert} role="alert">
           {msg}
         </p>
       )}
+
       {loggedIn && <CardContainer data={data} />}
     </>
   );
